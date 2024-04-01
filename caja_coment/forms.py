937fea34164ef264faca0bf15efcf65b7202d09f
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-04-01 18:59:27 -0300

    agregado de bootstrap

--- caja_coment/forms.py
+++ caja_coment/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 class CreateComentario(forms.Form):
-     nombre=forms.CharField(label="nombre", max_length=200)
-     mensaje=forms.CharField(label = "ingrese su comentario" ,widget=forms.Textarea)   
+     nombre=forms.CharField(label="Nombre", max_length=200)
+     mensaje=forms.CharField(label = "Ingrese su comentario" ,widget=forms.Textarea)   
